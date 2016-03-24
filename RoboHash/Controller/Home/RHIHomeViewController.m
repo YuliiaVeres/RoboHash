b1782c1034b59f02365663ad71848037236a4bff
Yuliia Veres <juliaveresklia@yahoo.com>
2016-03-24 12:06:38 +0200

    Added requests count info on Home Screen.

--- RoboHash/Controller/Home/RHIHomeViewController.m
+++ RoboHash/Controller/Home/RHIHomeViewController.m
@@ -9,12 +9,15 @@
 #import "RHIHomeViewController.h"
 #import "UIView+Cornered.h"
 #import "UINavigationBar+RoboHash.h"
-#import "RHIHistoryViewController.h"
+#import "RHIRoboHash.h"
+#import "RHICoreDataManager.h"
 
 @interface RHIHomeViewController ()
 
 @property (weak, nonatomic) IBOutlet UIButton *hashButton;
 @property (weak, nonatomic) IBOutlet UIButton *historyButton;
+
+@property (strong, nonatomic) NSManagedObjectContext *context;
 
 @end
 
@@ -32,12 +35,52 @@
     [self.navigationController setNavigationBarHidden:YES animated:YES];
 }
 
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 - (void)setup
 {
     [self.hashButton corneredWithRadius:5.0 borderColor:nil borderWidth:0.0];
     [self.historyButton corneredWithRadius:5.0 borderColor:nil borderWidth:0.0];
     
     [self.navigationController.navigationBar transparentBar];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateRequestsInfo:) name:NSManagedObjectContextDidSaveNotification object:nil];
+    
+    self.context = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
+    self.context.persistentStoreCoordinator = [RHICoreDataManager sharedInstance].persistentStoreCoordinator;
+    
+    [self executeFetch];
+}
+
+- (void)updateRequestsInfo:(NSNotification *)notification
+{
+    [self.context mergeChangesFromContextDidSaveNotification:notification];
+    [self executeFetch];
+}
+
+- (void)executeFetch
+{
+    __weak typeof(self)weakSelf = self;
+    
+    [self.context performBlockAndWait:^{
+        
+        typeof(weakSelf)strongSelf = weakSelf;
+        NSError *error = nil;
+        
+        NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:NSStringFromClass([RHIRoboHash class])];
+        NSArray *hashes = [strongSelf.context executeFetchRequest:request error:&error];
+        
+        if (error)
+            NSLog(@"Error merging home context : %@", error.localizedDescription);
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            [strongSelf.historyButton setTitle:[NSString stringWithFormat:@"%@ (%lu)", NSLocalizedString(@"Requests History", nil), (unsigned long)hashes.count] forState:UIControlStateNormal];
+        });
+    }];
 }
 
 @end
